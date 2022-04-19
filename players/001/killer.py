2f79849cd214130cdc575be989aa203b71396a3e
Jesus Sanchez <33048003+jedsaro@users.noreply.github.com>
2022-04-18 23:08:18 -0700

    ❤

--- players/001/killer.py
+++ players/001/killer.py
@@ -1,11 +1,28 @@
 import random
 
 def init():
-    return("💀")
+  return("🌮")
 
+def run(db_cursor , state): 
+  #get all my dots
+  i = 0
+  rows = db_cursor.execute(f"select x,y from main_game_field as gf, owner  where is_flag = FALSE and gf.owner_id = owner.owner_id and owner.name = '{state['NAME']}'")
+  
+  for row in rows.fetchall():
+    db_cursor.execute(logic(row)) 
+    
 
-def run(db_cursor , state):
-    #get all my dots
-    rows = db_cursor.execute(f"select x,y from main_game_field as gf, owner  where is_flag = FALSE and gf.owner_id = owner.owner_id and owner.name = '{state['NAME']}'")
-    for row in rows.fetchall():
-        db_cursor.execute(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0] + random.choice([0,1,-1]) }, {row[1] + random.choice([0,1,-1]) }, 'MOVE')")
+def logic(row):
+  
+  return(f"insert into engine_orders values( {row[0]}, {row[1]}, {left(row)}, {row[1] + 1 }, 'MOVE')")
+
+    
+def left(row):
+  return(row[0]-1)
+def right(row):
+  return(row[0]+1)
+def up(row):
+  return(row[1]-1)
+def down(row):
+  return(row[1]+1)
+
