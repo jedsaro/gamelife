88fc2a3e7df4cda0c5bc4baeac2f7bc430bc0f9c
jedsaro-Mapache <jedsaro@gmail.com>
2022-04-18 21:59:28 -0700

    🌮

--- players/001/killer.py
+++ players/001/killer.py
@@ -5,7 +5,25 @@ def init():
 
 
 def run(db_cursor , state):
+  
+  
     #get all my dots
-    rows = db_cursor.execute(f"select x,y from main_game_field as gf, owner  where is_flag = FALSE and gf.owner_id = owner.owner_id and owner.name = '{state['NAME']}'")
-    for row in rows.fetchall():
-        db_cursor.execute(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0] + random.choice([0,1,-1]) }, {row[1] + random.choice([0,1,-1]) }, 'MOVE')")
+  i = 0
+  rows = db_cursor.execute(f"select x,y from main_game_field as gf, owner  where is_flag = FALSE and gf.owner_id = owner.owner_id and owner.name = '{state['NAME']}'")
+  
+  for row in rows.fetchall():
+    db_cursor.execute(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0] - 1 }, {row[1] + 1 }, 'MOVE')") 
+    
+    
+def left(row):
+  return("row[0]-1")
+def right(row):
+  return("row[0]+1")
+def up(row):
+  return("row[0]-1")
+def down(row):
+  return("row[0]+1")
+# +1 down
+# -1 up
+# +1 right
+# -1 left
