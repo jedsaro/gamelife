3f06da278e17e11561aee66a2d5215aa5b1b82ae
jedsaro-Mapache <jedsaro@gmail.com>
2022-05-01 21:18:09 -0700

    🎶

--- players/001/agent.py
+++ players/001/agent.py
@@ -25,6 +25,8 @@ def down(row):
 
 def run(db_cursor, state):
 
+    print("DANONIO")
+
     food_location = []
 
     food = db_cursor.execute(
@@ -34,7 +36,9 @@ def run(db_cursor, state):
         food_location.append(i)
 
     food_location.sort()
-    
+
+    print(food_location)
+
     rows = db_cursor.execute(
         f"SELECT x,y from main_game_field as gf, owner where is_flag = FALSE and gf.owner_id = owner.owner_id and owner.name = '{state['NAME']}'")
 
@@ -46,12 +50,30 @@ def run(db_cursor, state):
 
 def logic(row, food_location):
 
-    print("PENE")
-    #find food
-    if row not in food_location: 
-      
+    if(row[0], row[1] != food_location[0][]):
+        # find the nearest food
+        nearest_food = food_location[0]
 
-        
+        for i in food_location:
+            if(abs(i[0] - row[0]) + abs(i[1] - row[1]) < abs(nearest_food[0] - row[0]) + abs(nearest_food[1] - row[1])):
+                nearest_food = i
+
+        # if the food is not in the same row
+        if(nearest_food[0] != row[0]):
+            # if the food is to the left
+            if(nearest_food[0] < row[0]):
+                return(f"insert into engine_orders values( {row[0]}, {row[1]}, {left(row)}, {row[1]}, 'MOVE')")
+            # if the food is to the right
+            else:
+                return(f"insert into engine_orders values( {row[0]}, {row[1]}, {right(row)}, {row[1]}, 'MOVE')")
+        # if the food is not in the same column
+        elif(nearest_food[1] != row[1]):
+            # if the food is above
+            if(nearest_food[1] < row[1]):
+                return(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0]}, {up(row)}, 'MOVE')")
+            # if the food is below
+            else:
+                return(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0]}, {down(row)}, 'MOVE')")
 
     return(f"insert into engine_orders values( {row[0]}, {row[1]}, {left(row)}, {down(row)}, 'MOVE')")
 
