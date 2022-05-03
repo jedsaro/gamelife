1ed5ebda8e4706852dae05666800f31704f3e5f1
jedsaro-Mapache <jedsaro@gmail.com>
2022-05-02 20:16:07 -0700

    🤷‍♀️ at leasta B

--- players/001/agent.py
+++ players/001/agent.py
@@ -46,32 +46,24 @@ def run(db_cursor, state):
 
 def logic(row, food_location):
 
-    if(row not in food_location):
-      
-        # find the nearest food
-        nearest_food = food_location[0]
-        print(nearest_food)
+    for looking in food_location: 
 
-        for i in food_location:
-            if(abs(i[0] - row[0]) + abs(i[1] - row[1]) < abs(nearest_food[0] - row[0]) + abs(nearest_food[1] - row[1])):
-                nearest_food = i
-
-        # if the food is not in the same row
-        if(nearest_food[0] != row[0]):
-            # if the food is to the left
-            if(nearest_food[0] < row[0]):
-                return(f"insert into engine_orders values( {row[0]}, {row[1]}, {left(row)}, {row[1]}, 'MOVE')")
-            # if the food is to the right
-            else:
-                return(f"insert into engine_orders values( {row[0]}, {row[1]}, {right(row)}, {row[1]}, 'MOVE')")
-        # if the food is not in the same column
-        elif(nearest_food[1] != row[1]):
-            # if the food is above
-            if(nearest_food[1] < row[1]):
-                return(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0]}, {up(row)}, 'MOVE')")
-            # if the food is below
-            else:
-                return(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0]}, {down(row)}, 'MOVE')")
+      # if the food is not in the same row
+      if(looking[0] != row[0]):
+          # if the food is to the left
+          if(looking[0] < row[0]):
+              return(f"insert into engine_orders values( {row[0]}, {row[1]}, {left(row)}, {row[1]}, 'MOVE')")
+          # if the food is to the right
+          else:
+              return(f"insert into engine_orders values( {row[0]}, {row[1]}, {right(row)}, {row[1]}, 'MOVE')")
+      # if the food is not in the same column
+      elif(looking[1] != row[1]):
+          # if the food is above
+          if(looking[1] < row[1]):
+              return(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0]}, {up(row)}, 'MOVE')")
+          # if the food is below
+          else:
+              return(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0]}, {down(row)}, 'MOVE')")
 
     return(f"insert into engine_orders values( {row[0]}, {row[1]}, {left(row)}, {down(row)}, 'MOVE')")
 
